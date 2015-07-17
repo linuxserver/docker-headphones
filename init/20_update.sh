e3ccdc96f62d1c831bf22cadec98a8e47e2d5834
Stian Buch Larsen <lonixx@gmail.com>
2015-07-17 08:04:36 +0200

    FIxed update command for headphones (its git pull not git checkout)

--- init/20_update.sh
+++ init/20_update.sh
@@ -3,6 +3,6 @@ if [ ! -d /opt/headphones/.git ]; then
 	git clone https://github.com/rembo10/headphones.git /opt/headphones
 else
 	cd /opt/headphones
-	git checkout master
+	git pull
 fi
 chown -R abc:abc /opt/headphones
