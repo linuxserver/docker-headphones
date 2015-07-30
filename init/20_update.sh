0fece1b9140d6f18a712f168c25a2a5c28c93e37
Stian Buch Larsen <lonixx@gmail.com>
2015-07-30 18:20:03 +0200

    running git as abc

--- init/20_update.sh
+++ init/20_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ ! -d /opt/headphones/.git ]; then 
-	git clone https://github.com/rembo10/headphones.git /opt/headphones
+	/sbin/setuser abc git clone https://github.com/rembo10/headphones.git /opt/headphones
 else
 	cd /opt/headphones
-	git pull
+	/sbin/setuser abc 	git pull
 fi
-chown -R abc:abc /opt/headphones
+#chown -R abc:abc /opt/headphones
 
 apt-get update -qq
 apt-get --only-upgrade install -yqq ffmpeg
