15a8f17adcb33e3793332a6742c8079eb7526cc6
Sean Dion <smdion@gmail.com>
2015-07-30 15:24:09 -0500

    remove running git as abc

--- init/20_update.sh
+++ init/20_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -d /opt/headphones/.git ]; then 
-	/sbin/setuser abc git clone https://github.com/rembo10/headphones.git /opt/headphones
+	git clone https://github.com/rembo10/headphones.git /opt/headphones
 else
 	cd /opt/headphones
-	/sbin/setuser abc git pull
+	git pull
 fi
 chown -R abc:abc /opt/headphones
