09f8ae55a54c57c67796126671558a8fcb96489e
Stian Buch Larsen <lonixx@gmail.com>
2015-07-30 18:21:38 +0200

    Move apt update to own file

--- init/20_update.sh
+++ init/20_update.sh
@@ -3,9 +3,6 @@ if [ ! -d /opt/headphones/.git ]; then
 	/sbin/setuser abc git clone https://github.com/rembo10/headphones.git /opt/headphones
 else
 	cd /opt/headphones
-	/sbin/setuser abc 	git pull
+	/sbin/setuser abc git pull
 fi
 #chown -R abc:abc /opt/headphones
-
-apt-get update -qq
-apt-get --only-upgrade install -yqq ffmpeg
